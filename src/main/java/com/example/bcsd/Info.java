e7da805860d267b9ded078ec23e5299017f1089b
pizzaa0930 <jwonpiseu@gmail.com>
2025-11-04 21:45:09 +0900

    feat:json으로 보낼 정보를 넣기 위해 java파일 생성

---
+++ src/main/java/com/example/bcsd/Info.java
@@ -0,0 +1,23 @@
+package com.example.bcsd;
+
+public class Info {
+    private int age;
+    private String name;
+
+    public Info(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
