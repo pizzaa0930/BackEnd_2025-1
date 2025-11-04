a77937f13e84ad73f38116352c24b306432c6c3f
pizzaa0930 <jwonpiseu@gmail.com>
2025-11-04 21:08:29 +0900

    fix:Responsebody 삭제

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -1,16 +1,19 @@
 package com.example.bcsd;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-    @ResponseBody
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!!!!!";
+
+    @GetMapping("/introduce")
+    public String introduce(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "introduce";
     }
 
     @GetMapping("/hello2")
