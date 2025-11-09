da803f4557d5a5f575b7ae7b0df05dee78c22fd2
pizzaa0930 <jwonpiseu@gmail.com>
2025-11-09 17:16:24 +0900

    feat: map생성

--- src/main/java/com/example/bcsd/HelloController.java
+++ src/main/java/com/example/bcsd/HelloController.java
@@ -1,11 +1,16 @@
 package com.example.bcsd;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/article")
@@ -21,5 +26,16 @@ public class HelloController {
     @GetMapping("/hello2")
     public String hello2() {
         return "hello";
+    }
+
+    private Map<Integer,Article> articleMap = new HashMap<>();
+    private int newId = 1;
+
+    @PostMapping
+    public ResponseEntity<Article> postArticle(@RequestBody Article article) {
+        article.setId(newId++);
+        articleMap.put(article.getId(), article);
+
+        return new ResponseEntity<>(article,HttpStatus.CREATED);
     }
 }
