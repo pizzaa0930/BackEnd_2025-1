c0ad65b76a88e8d36bd33b92b183a08ccc4bfd2a
pizzaa0930 <jwonpiseu@gmail.com>
2025-11-16 19:46:33 +0900

    refactor: model 패키지를 만들고 이동

---
+++ src/main/java/com/example/bcsd/model/Article.java
@@ -0,0 +1,15 @@
+package com.example.bcsd.model;
+
+public class Article {
+    private Long id;
+    private String authorID;
+    private String boardID;
+    private String title;
+    private String description;
+    private String createDay;
+    private String updateDay;
+
+
+
+
+}
