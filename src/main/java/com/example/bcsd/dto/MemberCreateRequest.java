7d21d68c17d80baa93683ed314f44f73ec5adef6
pizzaa0930 <jwonpiseu@gmail.com>
2025-12-29 20:50:49 +0900

    feat:DTO 추가

---
+++ src/main/java/com/example/bcsd/dto/MemberCreateRequest.java
@@ -0,0 +1,20 @@
+package com.example.bcsd.dto;
+
+public class MemberCreateRequest {
+
+    private String name;
+    private String email;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/com/example/bcsd/dto/MemberUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.example.bcsd.dto;
+
+public class MemberUpdateRequest {
+
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
