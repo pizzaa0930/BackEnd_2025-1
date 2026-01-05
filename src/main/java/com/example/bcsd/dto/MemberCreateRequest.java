464544e5ccea7bad1c90bde7a8d0765b3af3a64f
pizzaa0930 <jwonpiseu@gmail.com>
2026-01-05 20:20:59 +0900

    feat:회원가입 추가

--- src/main/java/com/example/bcsd/dto/MemberCreateRequest.java
+++ src/main/java/com/example/bcsd/dto/MemberCreateRequest.java
@@ -1,10 +1,20 @@
 package com.example.bcsd.dto;
 
+import com.example.bcsd.model.Member;
+
 public class MemberCreateRequest {
 
     private String name;
     private String email;
     private String password;
+
+    public MemberCreateRequest() {}
+
+    public MemberCreateRequest(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 
     public String getName() {
         return name;
@@ -16,5 +26,9 @@ public class MemberCreateRequest {
 
     public String getPassword() {
         return password;
+    }
+
+    public Member toEntity() {
+        return new Member(name, email, password);
     }
 }
